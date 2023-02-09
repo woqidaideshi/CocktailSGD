5465094f2595ee6458d604ebab464a540324b9ab
Ubuntu <yafen.fang@inf.ethz.ch>
2023-02-09 16:00:01 +0000

    test on AWS nodes

--- example_scripts/pretrain_opt125m_p2ptun_colink.sh
+++ example_scripts/pretrain_opt125m_p2ptun_colink.sh
@@ -1,4 +1,4 @@
-netif=ens5
+netif=clba453141
 export GLOO_SOCKET_IFNAME=${netif}
 export NCCL_SOCKET_IFNAME=${netif}
 export WANDB_NAME=opt-125m-cot
@@ -12,17 +12,17 @@ cd ../
 ARGS="--model-name ./empty_model_configs/opt-125m \
 --tokenizer-name ./empty_model_configs/opt-125m \
 --load-pretrained-model false \
---project-name cocktail-allreduce \
+--project-name cocktail-allreduce-p2p \
 --model-type opt \
 --optimizer adam \
 --seed 42 \
 --task-name cot \
---checkpoint-path ./model_ckpts/$WANDB_NAME \
+--checkpoint-path ./model_ckpts/$WANDB_NAME-colink \
 --num-layers 12 --embedding-dim 768 \
 --total-steps 200 --warmup-steps 10 --train-warmup-steps 0 \
 --checkpoint-steps 100 \
 --lr 1e-4 --seq-length 2048 --batch-size 16 --micro-batch-size 1 --gradient-accumulate-step 1 \
---dist-url tcp://3.126.245.86:7033 \
+--dist-url tcp://10.133.70.2:5000 \
 --world-size 3 --pipeline-group-size 1 --data-group-size 3 \
 --job-id 0 --net-interface ${netif} \
 --fp16 \
@@ -42,3 +42,4 @@ python3 dist_lm_train.py $(echo ${ARGS}) --cuda-id 0 --rank 0 \
 # python dist_lm_train.py $(echo ${ARGS}) --cuda-id 7 --rank 3 \
 #     & \
 wait)
+
