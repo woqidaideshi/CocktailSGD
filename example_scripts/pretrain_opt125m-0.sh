d06c5eef921afba1cbafd5f52382964d21759735
Ubuntu <yafen.fang@inf.ethz.ch>
2023-02-09 16:00:01 +0000

    test on AWS nodes

--- example_scripts/pretrain_opt125m-0.sh
+++ example_scripts/pretrain_opt125m-0.sh
@@ -12,18 +12,18 @@ cd ../
 ARGS="--model-name ./empty_model_configs/opt-125m \
 --tokenizer-name ./empty_model_configs/opt-125m \
 --load-pretrained-model false \
---project-name cocktail-allreduce \
+--project-name cocktail-allreduce-node1-testbase \
 --model-type opt \
 --optimizer adam \
 --seed 42 \
 --task-name cot \
---checkpoint-path ./model_ckpts/$WANDB_NAME \
+--checkpoint-path ./model_ckpts/$WANDB_NAME-testbase \
 --num-layers 12 --embedding-dim 768 \
 --total-steps 200 --warmup-steps 10 --train-warmup-steps 0 \
 --checkpoint-steps 100 \
 --lr 1e-4 --seq-length 2048 --batch-size 16 --micro-batch-size 1 --gradient-accumulate-step 1 \
---dist-url tcp://3.126.245.86:7033 \
---world-size 3 --pipeline-group-size 1 --data-group-size 3 \
+--dist-url tcp://127.0.0.1:7033 \
+--world-size 1 --pipeline-group-size 1 --data-group-size 1 \
 --job-id 0 --net-interface ${netif} \
 --fp16 \
 --dp-backend gloo \
@@ -42,3 +42,4 @@ python3 dist_lm_train.py $(echo ${ARGS}) --cuda-id 0 --rank 0 \
 # python dist_lm_train.py $(echo ${ARGS}) --cuda-id 7 --rank 3 \
 #     & \
 wait)
+
